8917d1fb38d2cd57659591d9276d4181c9829e3d
Iskander Nafikov <nafikov.iskander@mail.ru>
2026-09-17 00:56:36 +0300

    fix(zsh): remove check of nix and profile bins on PATH for macOS

--- files/zsh/environment.zsh
+++ files/zsh/environment.zsh
@@ -6,11 +6,14 @@ setopt HIST_REDUCE_BLANKS
 setopt INC_APPEND_HISTORY
 setopt SHARE_HISTORY
 
-# Nix profile environment (ensure nix + profile bins are on PATH)
-if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
-  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-elif [ -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]; then
-  . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+# Nix bootstrap for Linux.
+# On macOS, nix-darwin configures the Nix environment system-wide.
+if [[ "$OSTYPE" == linux* ]]; then
+  if [ -e /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh ]; then
+    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  elif [ -e /nix/var/nix/profiles/default/etc/profile.d/nix.sh ]; then
+    . /nix/var/nix/profiles/default/etc/profile.d/nix.sh
+  fi
 fi
 
 # Prompt config
